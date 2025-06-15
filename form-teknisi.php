97b9c69cdd0b66b36a775d7ae370e9a8675c582d
citynet1 <33652827+citynet1@users.noreply.github.com>
2025-06-16 06:36:35 +0700

    Update form-teknisi.php

--- form-teknisi.php
+++ form-teknisi.php
@@ -46,6 +46,8 @@
               </div>
               <div class="mb-3">
                 <label class="form-label">Teknisi</label>
+                </div>
+              <div class="mb-3">
                     <div class="form-check">
                     <input class="form-check-input" type="checkbox" name="teknisi[]" value="Iman" id="teknisiA">
                     <label class="form-check-label" for="teknisiA">Sdidk</label>
